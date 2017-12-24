3bc2f0f60fb11c3e30e0509a67e66397db3dc315
John Hancock <jhancock1975@gmail.com>
2017-12-24 13:48:36 -0500

    various changes while reading tutorial; chnage to use support api instead of framework

--- app/src/main/java/com/kewlala/myfirstapp/MainActivity.java
+++ app/src/main/java/com/kewlala/myfirstapp/MainActivity.java
@@ -2,8 +2,8 @@ package com.kewlala.myfirstapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.text.BidiFormatter;
 import android.support.v7.app.AppCompatActivity;
-import android.text.BidiFormatter;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -25,14 +25,24 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.activityMainHelloWorldTxt);
         BidiFormatter bidiFormatter = BidiFormatter.getInstance();
         String helloWorld = getResources().getString(R.string.hello_world);
+        //always use BidiFormatter.unicodeWrap for strings to
+        //get proper left-to-right or right-to-left formatting
+
+        //always use string.format for numbers to properly
+        //render numbers
         textView.setText(bidiFormatter.unicodeWrap(helloWorld));
+
+
     }
+
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
+
+
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
